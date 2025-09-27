72ada20abefe292b292cd03f580e384cebd9e76f
Luka Jacobowitz <luka.jacobowitz@gmail.com>
2025-09-26 22:25:55 -0400

    Force .js import format using eslint (#117)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 
 import eslint from "@eslint/js"
 import tseslint from "typescript-eslint"
+import pluginImport from "eslint-plugin-import"
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -13,10 +14,21 @@ export default tseslint.config(
         project: "./tsconfig.json",
       },
     },
+    plugins: {
+      import: pluginImport,
+    },
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/require-await": "off",
       "@typescript-eslint/restrict-template-expressions": "off",
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          js: "always",
+          ts: "never",
+        },
+      ],
     },
   },
 )
